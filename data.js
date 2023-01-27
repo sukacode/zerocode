554a8b28baffedca51c24855468065349c48e472
sukacode <firmansahalhadi@gmail.com>
2023-01-27 22:46:51 +0700

    update-js

--- data.js
+++ data.js
@@ -1,15 +1,22 @@
 const nama = "ilhamalfir";
-let usia = 30;
+let usia = 15;
 
 function generateBiodata() {
-  if (usia == 30) {
-    //first condition
-    console.log("Anda Dewasa");
+  let generasi;
+
+  if (usia > 10 && usia < 18) {
+    generasi = "generasi remaja";
+  } else if (usia > 18 && usia < 30) {
+    generasi = "generasi dewasa";
+  } else if (usia > 30) {
+    generasi = "generasi tua";
+  } else if (usia < 10 && usia > 2) {
+    generasi = "generasi anak anak";
   } else {
-    //jika kondisi tidak terpenuh
-    console.log("Anda anak anak");
+    generasi = "generasi bayi";
   }
-  console.log(`nama saya adalah ${nama} dan usia saya ${usia} tahun`);
+
+  return console.log("generasi saya adalah", generasi);
 }
 
 console.log(nama);
